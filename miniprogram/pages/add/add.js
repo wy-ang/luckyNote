f2db314e0118185ae02c6c05668dee518d1b265c
wy-ang <wyang1117@msn.com>
2019-10-16 00:33:53 +0800

    替换form为富文本编辑器

--- miniprogram/pages/add/add.js
+++ miniprogram/pages/add/add.js
@@ -1,9 +1,13 @@
 Page({
+  data: {
+    formats: {},
+    placeholder: '开始输入...',
+  },
   onLoad: function(option) {
     //读取数据库
     const that = this;
     const content = option.content !== 'undefined' ? option.content : '';
-    console.log(option)
+    console.log(content)
     const type = option.type;
     const id = option.id;
     const db = wx.cloud.database();
@@ -30,6 +34,95 @@ Page({
       },
     })
   },
+  onStatusChange(e) {
+    const formats = e.detail
+    this.setData({
+      formats
+    })
+  },
+  onEditorReady() {
+    const that = this
+    wx.createSelectorQuery().select('#editor').context(function(res) {
+      that.editorCtx = res.context
+      that.editorCtx.setContents({
+        delta: that.data.content,
+        success: (res) => {
+          console.log(res)
+        },
+        fail: (res) => {
+          console.log(res)
+        }
+      })
+    }).exec();
+  },
+  // 修改样式
+  format(e) {
+    let {
+      name,
+      value
+    } = e.target.dataset
+    if (!name) return
+    // console.log('format', name, value)
+    this.editorCtx.format(name, value)
+  },
+  // 插入图片
+  insertImage() {
+    const that = this
+    wx.chooseImage({
+      count: 1,
+      success: function(res) {
+        that.editorCtx.insertImage({
+          src: res.tempFilePaths[0],
+          data: {
+            id: 'abcd',
+            role: 'god'
+          },
+          width: '80%',
+          success: function() {
+            console.log('insert image success')
+          }
+        })
+      }
+    })
+  },
+  // 编辑器失焦，同时收起键盘
+  blur() {
+    this.editorCtx.blur()
+  },
+  submit() {
+    this.editorCtx.getContents({
+      success: (res) => {
+        const content = res.delta;
+        const db = wx.cloud.database();
+        db.collection('notes').add({
+          data: {
+            content,
+          },
+          success: res => {
+            // 在返回结果中会包含新创建的记录的 _id
+            this.setData({
+              content
+            })
+            wx.showToast({
+              title: '新增记录成功',
+            })
+            wx.redirectTo({
+              url: '../index/index'
+            })
+          },
+          fail: err => {
+            wx.showToast({
+              icon: 'none',
+              title: '新增记录失败'
+            })
+          }
+        })
+      },
+      fail: (res) => {
+        console.log(res)
+      },
+    })
+  },
   //操作数据库
   res: function(e) {
     const type = e.detail.target.dataset.type;
@@ -53,7 +146,6 @@ Page({
       db.collection('notes').add({
         data: {
           content,
-          tempFilePaths,
         },
         success: res => {
           // 在返回结果中会包含新创建的记录的 _id
@@ -76,55 +168,9 @@ Page({
       })
     }
   },
-
   goBack(event) {
     wx.navigateBack({
       delta: 1
-    })
-  },
-  // 选择图片
-  chooseImage() {
-    var that = this;
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album'],
-      success: function(res) {
-        const tempFilePaths = res.tempFilePaths;
-        wx.showLoading({
-          title: '上传中',
-        })
-        // 上传图片
-        for (let i = 0; i < tempFilePaths.length; i++) {
-          wx.cloud.uploadFile({
-            cloudPath: 'images' + tempFilePaths[i].match(/\.[^.]+?$/)[0],
-            filePath: tempFilePaths[i],
-            success: res => {
-              console.log('[上传文件] 成功：', res)
-              app.globalData.fileID = res.fileID
-              app.globalData.cloudPath = cloudPath
-              app.globalData.imagePath = filePath
-
-              wx.navigateTo({
-                url: '../storageConsole/storageConsole'
-              })
-            },
-            fail: e => {
-              console.error('[上传文件] 失败：', e)
-              wx.showToast({
-                icon: 'none',
-                title: '上传失败',
-              })
-            },
-            complete: () => {
-              wx.hideLoading()
-            }
-          })
-        }
-      },
-      fail: e => {
-        console.error(e)
-      }
     })
   },
   // 拍照
